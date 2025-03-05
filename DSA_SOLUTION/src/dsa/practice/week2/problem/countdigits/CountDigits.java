311ee35f6c06778cd7e8966b5f8fc490468d6fbc
yuvaprakashm <yuvaprakashmahalingam2001@gmail.com>
2025-03-05 15:13:21 +0530

    added questions

--- DSA_SOLUTION/src/dsa/practice/week2/problem/countdigits/CountDigits.java
+++ DSA_SOLUTION/src/dsa/practice/week2/problem/countdigits/CountDigits.java
@@ -1,3 +1,28 @@
+/* Write a program that gets n as input and print the number of digits in the number
+
+Testcase 1 :
+
+Input : 
+
+325345
+
+Expected output:
+
+6
+
+
+
+Testcase 2 :
+
+Input : 
+
+9879
+
+Expected output:
+
+4 
+
+ */
 package dsa.practice.week2.problem.countdigits;
 
 public class CountDigits {
