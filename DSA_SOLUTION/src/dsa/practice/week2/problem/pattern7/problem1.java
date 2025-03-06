297d46041917c1d3f42795c9f4b47f58a48de5f9
yuvaprakashm <yuvaprakashmahalingam2001@gmail.com>
2025-03-06 11:05:30 +0530

    update in problem 7

--- DSA_SOLUTION/src/dsa/practice/week2/problem/pattern7/problem1.java
+++ DSA_SOLUTION/src/dsa/practice/week2/problem/pattern7/problem1.java
@@ -45,8 +45,8 @@ package dsa.practice.week2.problem.pattern7;
 public class problem1 {
 	public static void main(String[] args) {
 		int n = 5;
-		for(int i = n; i >= 1; i--) {
-			for(int j = i; j >= 1; j--) {
+		for(int i = 1; i <= n; i++) {
+			for(int j = n; j >= i ; j--) {
 				System.out.print(j);
 			}
 			System.out.println();
